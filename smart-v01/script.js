4ef3b0988b0d8a18cf45a3bdb8be42a364bde1f6
e-o-o <111095607+e-o-o@users.noreply.github.com>
2022-09-18 14:23:36 +0200

    Add files via upload

---
+++ smart-v01/script.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById("myModal");
+var body = document.getElementsByTagName("body")[0];
+var btn = document.getElementById("myBtn");
+btn.onclick = function () {
+  modal.style.display = "block";
+  body.setAttribute("style", "-webkit-backdrop-filter:none;backdrop-filter: none;");
+  document.querySelector("#main-background").style.backgroundImage = "url('"+dataStyle.background+"')";
+  document.querySelector("#main-background").style.backgroundColor = ""+dataStyle.background_color+"";
+};
+// ************************************************************************************************* //
+/* ----------------main items -------------------------*/
+document.title = dataStyle.page_title;
+/* ----------------Images -----------------------------*/
+document.querySelector("#main-logo").src = dataStyle.logo;
+document.querySelector("#main-img").src = dataStyle.img;
+/* ----------------texts -----------------------------*/
+document.querySelector("#main-text").innerHTML = dataStyle.heading;
+document.querySelector("#text").innerHTML = dataStyle.paragraph;
+/* -----------------------------------------------------*/
+document.querySelector("#main-background").style.backgroundImage = "url('"+dataStyle.background+"')";
+document.querySelector("#main-background").style.backgroundColor = ""+dataStyle.background_color+"";
+/* -------------------------------------------------------*/
+document.querySelector("#main-text").style.color = dataStyle.heading_color;
+document.querySelector("#text").style.color = dataStyle.paragraph_color;
+
+// ************************************************************************************************* //
